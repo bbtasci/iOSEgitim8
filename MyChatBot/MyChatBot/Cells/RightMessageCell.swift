d269cd097ad374babd3a0a63619a2bb828a1772e
Baris Berkin Tasci <barisberkin.tasci@hotmail.com>
2020-12-25 03:48:46 +0300

    Right sender is done.

---
+++ MyChatBot/MyChatBot/Cells/RightMessageCell.swift
@@ -0,0 +1,35 @@
+//
+//  RightMessageCell.swift
+//  MyChatBot
+//
+//  Created by Baris Berkin Tasci on 25.12.2020.
+//
+
+import UIKit
+
+final class RightMessageCell: UITableViewCell {
+    
+    @IBOutlet weak var rightMessageBackgroundView: UIView!
+    @IBOutlet weak var rightMessageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func updateRightMessageCell (by message: RightMessageData) {
+        rightMessageBackgroundView.layer.cornerRadius = 10
+        rightMessageBackgroundView.clipsToBounds = true
+        
+        rightMessageLabel.text = message.text
+        rightMessageLabel.textAlignment = .right
+    }
+    
+}
+
---
+++ MyChatBot/MyChatBot/Models/RightMessageData.swift
@@ -0,0 +1,10 @@
+//
+//  RightMessageData.swift
+//  MyChatBot
+//
+//  Created by Baris Berkin Tasci on 24.12.2020.
+//
+
+struct RightMessageData {
+    let text: String
+}
